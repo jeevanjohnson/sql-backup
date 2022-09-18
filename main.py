79db07639e6f645093bc0105a14f6fbda7309f8a
cmyui <cmyuiosu@gmail.com>
2022-09-18 01:24:45 -0400

    refactor: functionality into distinct functions

--- main.py
+++ main.py
@@ -51,65 +51,100 @@ def magnitude_time_format(seconds: float) -> str:
     raise NotImplementedError("Time too large")
 
 
-def remove_if_exists(filename: str) -> None:
-    if os.path.exists(filename):
-        os.remove(filename)
+async def export_sql_to_file(
+    file_name: str,
+    db_user: str,
+    db_pass: str,
+    db_name: str,
+) -> int:
+    with open(file_name, "wb+") as backup_file:
+        process = await asyncio.subprocess.create_subprocess_shell(
+        f"sudo mysqldump -u {db_user} -p{db_pass} {db_name}",
+        stdout=backup_file,
+        stderr=asyncio.subprocess.PIPE,
+    )
+        # make sure all data is flushed
+        _, stderr = await process.communicate()
+
+    if process.stderr is not None:
+        logging.error(stderr.decode())
+
+    if process.returncode is None:
+        return 1
+
+    return process.returncode
+
+
+async def upload_file_to_s3(
+    file_name: str,
+    region_name: str,
+    endpoint_url: str,
+    aws_access_key_id: str,
+    aws_secret_access_key: str,
+    bucket_name: str,
+) -> None:
+    session = get_session()  # TODO: env vars?
+    with open(file_name, "rb") as backup_file:
+        async with session.create_client(
+            service_name="s3",
+            region_name=region_name,
+            endpoint_url=endpoint_url,
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
+        ) as s3_client:
+            await s3_client.put_object(
+                Bucket=bucket_name,
+                Key=f"db-backups/{file_name}",
+                Body=backup_file,
+            )
+
+
+def remove_if_exists(file_name: str) -> None:
+    if os.path.exists(file_name):
+        os.remove(file_name)
 
 
 async def main() -> int:
     start_time = time.perf_counter()
-    backup_filename = f"backup_{datetime.now().isoformat()}.sql"
+    backup_file_name = f"backup_{datetime.now().isoformat()}.sql"
 
     # make sure the backup is removed from disk
-    atexit.register(remove_if_exists, backup_filename)
+    atexit.register(remove_if_exists, backup_file_name)
 
-    with open(backup_filename, "wb+") as backup_file:
-        process = await asyncio.subprocess.create_subprocess_shell(
-        f"sudo mysqldump -u {DB_USER} -p{DB_PASS} {DB_NAME}",
-        stdout=backup_file,
-        stderr=asyncio.subprocess.PIPE,
+    export_exit_code = await export_sql_to_file(
+        db_user=DB_USER,
+        db_pass=DB_PASS,
+        db_name=DB_NAME,
+        file_name=backup_file_name,
     )
-        _, stderr = await process.communicate()
 
-    exit_code = process.returncode
-    assert exit_code is not None
-    if exit_code != 0:
-        if process.stderr is not None:
-            logging.error(stderr.decode())
-        return exit_code
+    if export_exit_code != 0:
+        return export_exit_code
 
     try:
-        session = get_session()  # TODO: env vars?
-        with open(backup_filename, "rb") as backup_file:
-            async with session.create_client(
-                service_name="s3",
-                region_name=AWS_BUCKET_REGION,
-                endpoint_url=AWS_ENDPOINT_URL,
-                aws_access_key_id=AWS_ACCESS_KEY_ID,
-                aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-            ) as s3_client:
-                await s3_client.put_object(
-                    Bucket=AWS_BUCKET_NAME,
-                    Key=f"db-backups/{backup_filename}",
-                    Body=backup_file,
-                )
+        await upload_file_to_s3(
+            file_name=backup_file_name,
+            region_name=AWS_BUCKET_REGION,
+            endpoint_url=AWS_ENDPOINT_URL,
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+            bucket_name=AWS_BUCKET_NAME,
+        )
     except Exception as e:
-        logging.error(f"{backup_filename} failed uploading to bucket")
+        logging.error(f"{backup_file_name} failed uploading to bucket")
         logging.error(e)
         return 1
 
     # get file stats for backup file on disk
-    stat_result = os.stat(backup_filename)
+    stat_result = os.stat(backup_file_name)
 
     file_size = magnitude_format_size(stat_result.st_size)
     time_elapsed = magnitude_time_format(time.perf_counter() - start_time)
-    logging.info(f"{backup_filename} ({file_size}) uploaded in {time_elapsed}")
+    logging.info(f"{backup_file_name} ({file_size}) uploaded in {time_elapsed}")
 
     return 0
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=LOG_LEVEL)
-
-    exit_code = asyncio.run(main())
-    raise SystemExit(exit_code)
+    raise SystemExit(asyncio.run(main()))
